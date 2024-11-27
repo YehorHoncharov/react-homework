0bec7d9d08675ae239ff73271062329726988e32
Yehor Honcharov <honcharovstallker@gmail.com>
2024-11-27 19:08:12 +0200

    add likes and dislikes

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,22 +1,59 @@
-import './style.css'
+import { useState } from "react";
+import "./style.css";
 
 interface IPostProps {
-    title: string,
-    description: string,
-    img: string,
-    author: string,
+  title: string;
+  description: string;
+  img: string;
+  author: string;
+  like: string;
+  dislike: string;
 }
 
-export function Post(props: IPostProps){
-    return(
-        <div className='main-div'>
-            <div className='div'>
-                <h1 className='center-h1'>Назва: {props.title}</h1>
-                <p className='center-p'>Опис: {props.description}</p>
-                <img className='center-img' src={props.img} alt="" />
-                <p className='center-p'>Автор: {props.author}</p>
-            </div>
-        </div>
-    )
+export function Post(props: IPostProps) {
+  const [like, setLike] = useState(0)
+  const [dislike, setDislike] = useState(0)
+  const [isLiked, setIsLiked] = useState(false)
+  const [isDisliked, setIsDisliked] = useState(false)
+
+  function incrementLike() {
+    if (!isLiked) {
+      setLike(like+1);
+      setIsLiked(true);
+
+
+      if (isDisliked) {
+        setDislike(dislike-1);
+        setIsDisliked(false);
+      }
+    }
+  }
+
+  function incrementDislike() {
+    if (!isDisliked) {
+      setDislike(dislike+1);
+      setIsDisliked(true);
+
+      
+      if (isLiked) {
+        setLike(like-1);
+        setIsLiked(false);
+      }
+    }
+  }
+
+  return (
+    <div className="main-div">
+      <div className="div">
+        <h1 className="center-h1">Назва: {props.title}</h1>
+        <p className="center-p">Опис: {props.description}</p>
+        <img className="center-img" src={props.img} alt="" />
+        <p className="center-p">Автор: {props.author}</p>
+        <p>Likes: {like}</p>
+        <p>Dislikes: {dislike}</p>
+        <button disabled={isLiked} onClick={incrementLike}><img className="like" src={props.like} alt="" /></button>
+        <button disabled={isDisliked} onClick={incrementDislike}><img className="dislike" src={props.dislike} alt="" /></button>
+      </div>
+    </div>
+  );
 }
-// https://ae04.alicdn.com/kf/Hb55cdf7f5b3c4ff698552dd48336f5d5l.jpg_640x640.jpg
--- src/PostList.tsx
+++ src/PostList.tsx
@@ -9,6 +9,8 @@ export function PostList() {
           description: 'Этот пост познакомит вас с основами React, его компонентной архитектурой и JSX.',
           image: 'https://ae04.alicdn.com/kf/Hb55cdf7f5b3c4ff698552dd48336f5d5l.jpg_640x640.jpg',
           author: 'Иван Иванов',
+          imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
+          imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
         },
         {
           id: 1,
@@ -16,6 +18,8 @@ export function PostList() {
           description: 'Рассмотрим основные хуки React, такие как useState и useEffect, и их применение.',
           image: 'https://ae04.alicdn.com/kf/Hb55cdf7f5b3c4ff698552dd48336f5d5l.jpg_640x640.jpg',
           author: 'Мария Петрова',
+          imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
+          imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
         },
         {
           id: 2,
@@ -23,17 +27,22 @@ export function PostList() {
           description: 'Этот пост поможет вам понять, как интегрировать TypeScript в ваш проект на React для типизации и улучшения кода.',
           image: 'https://ae04.alicdn.com/kf/Hb55cdf7f5b3c4ff698552dd48336f5d5l.jpg_640x640.jpg',
           author: 'Сергей Смирнов',
+          imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
+          imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
         },
       ];
+
       return (
         <div>
           {posts.map((post) => (
             <Post
-              key = {post.id}
+              key={post.id}
               title={post.title}
               description={post.description}
               img={post.image}
               author={post.author}
+              like={post.imgLike}
+              dislike={post.imgDislike}
             />
           ))}
         </div>
