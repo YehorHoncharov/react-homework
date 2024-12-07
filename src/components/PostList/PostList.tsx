42ae267e523e2f20ac1857df163601a04101c111
Yehor Honcharov <honcharovstallker@gmail.com>
2024-12-07 22:53:21 +0200

    add filtered category

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,41 +1,86 @@
+import { useEffect, useState } from "react";
 import { Post } from "../Post/Post";
 // import { App } from "./App";
 import './PostList.css'
 
-export function PostList() {
-    const posts = [
-        {
-          id: 0,
-          title: 'Введение в React',
-          description: 'lorem ipsum dolor sit amet',
-          image: 'https://ru.legacy.reactjs.org/logo-og.png',
-          author: 'Иван Иванов',
-          imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
-          imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
-        },
-        {
-          id: 1,
-          title: 'Введение в React',
-          description: 'lorem ipsum dolor sit amet',
-          image: 'https://ru.legacy.reactjs.org/logo-og.png',
-          author: 'Мария Петрова',
-          imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
-          imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
-        },
-        {
-          id: 2,
-          title: 'Введение в React',
-          description: 'lorem ipsum dolor sit amet',
-          image: 'https://ru.legacy.reactjs.org/logo-og.png',
-          author: 'Сергей Смирнов',
-          imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
-          imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
-        },
-      ];
+const posts = [
+  {
+    id: 0,
+    category: 'cats',
+    title: 'Введение в React',
+    description: 'lorem ipsum dolor sit amet',
+    image: 'https://ru.legacy.reactjs.org/logo-og.png',
+    author: 'Иван Иванов',
+    imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
+    imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
+  },
+  {
+    id: 1,
+    category: 'bobik',
+    title: 'Введение в React',
+    description: 'lorem ipsum dolor sit amet',
+    image: 'https://ru.legacy.reactjs.org/logo-og.png',
+    author: 'Мария Петрова',
+    imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
+    imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
+  },
+  {
+    id: 2,
+    category: 'dogs',
+    title: 'Введение в React',
+    description: 'lorem ipsum dolor sit amet',
+    image: 'https://ru.legacy.reactjs.org/logo-og.png',
+    author: 'Сергей Смирнов',
+    imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
+    imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
+  },
+  {
+    id: 4,
+    category: 'chebureki',
+    title: 'Введение в React',
+    description: 'lorem ipsum dolor sit amet',
+    image: 'https://ru.legacy.reactjs.org/logo-og.png',
+    author: 'Сергей Смирнов',
+    imgLike: "https://cdn3.emoji.gg/emojis/8750-like.png",
+    imgDislike: "https://img1.picmix.com/output/stamp/thumb/9/7/4/2/2622479_884dc.png"
+  },
+];
 
-      return (
+export function PostList() {
+    const [filteredPosts, setFilteredPosts] = useState(posts)
+    const [selectedCategory, setSelectedCategory] = useState('All')
+
+    useEffect(()=>{
+        if(selectedCategory === 'All'){
+            setFilteredPosts(posts)
+        } else{
+            setFilteredPosts(posts.filter((post)=>{
+                return post.category === selectedCategory
+            }))
+        }
+    }, [selectedCategory])
+
+
+    return (
+      <>
+        
         <div className="postList">
-          {posts.map((post) => (
+          <div className="selectedCategory">
+          <select
+            onChange={(event) => {
+              setSelectedCategory(event.target.value);
+            }}
+          >
+            <option value="All">All</option>
+            <option value="cats">Cats</option>
+            <option value="bobik">Chat-Bot</option>
+            <option value="dogs">Dogs</option>
+            <option value="chebureki">Chebureki</option>
+          </select>
+          </div>
+    
+        {/* <div className="postList"> */}
+          {filteredPosts.map((post) => (
             <Post
               key={post.id}
               title={post.title}
@@ -47,7 +92,8 @@ export function PostList() {
             />
           ))}
         </div>
-      );
+      </>
+    );
 }
 
 // https://cdn3.emoji.gg/emojis/8750-like.png
